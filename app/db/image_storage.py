afc94107d7da4febb9acc2fd94ffd7420904f0c3
Yufeng-94 <ericteng94@gmail.com>
2025-09-26 15:35:37 -0400

    update image storage

--- app/db/image_storage.py
+++ app/db/image_storage.py
@@ -1,19 +1,37 @@
+from typing import List
+import os
+import shutil
 
 class ImageStorage:
     """
     <Description>
     """
-    def __init__(self):
-        pass
+    def __init__(self, image_dir: str):
 
-    def upload_images(self):
+        self.image_dir = image_dir
+
+        # Create image directory if not exists
+        if not os.path.exists(self.image_dir):
+            os.mkdir(self.image_dir)
+
+    def upload_images(self, image_paths: List[str]) -> List[str]:
         # save images to storage
+        returned_image_urls = []
+        for p in image_paths:
+            image_url = shutil.copy(p, self.image_dir)
+            returned_image_urls.append(image_url)
 
         # return image URLs
-        pass
+        return returned_image_urls
 
-    def query_images(self):
-        pass
+    def query_images(self, image_urls: List[str]) -> List[str]:
+        """
+        Given a list of image URLs, return the valid image URLs that exist in storage.
+        """
+        # Remove None
+        valid_image_urls = [url for url in image_urls if url]
 
-    def _save_to_storage(self):
-        pass
+        # Check if image exists in storage
+        existing_image_urls = [url for url in valid_image_urls if os.path.exists(url)]
+
+        return existing_image_urls
