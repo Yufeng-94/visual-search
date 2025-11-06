678f534119a96b78f0e794b893d422c0759c5d63
Yufeng-94 <ericteng94@gmail.com>
2025-11-05 23:59:10 -0500

    move proto to shared contracts

--- shared_contracts/shared_contracts/protos/image_encoding_service/image_encoding_service_pb2_grpc.py
+++ shared_contracts/shared_contracts/protos/image_encoding_service/image_encoding_service_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from proto import image_encoding_service_pb2 as proto_dot_image__encoding__service__pb2
+from shared_contracts.protos.image_encoding_service import image_encoding_service_pb2 as shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2
 
 GRPC_GENERATED_VERSION = '1.75.1'
 GRPC_VERSION = grpc.__version__
@@ -18,7 +18,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in proto/image_encoding_service_pb2_grpc.py depends on'
+        + f' but the generated code in shared_contracts/protos/image_encoding_service/image_encoding_service_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
@@ -36,8 +36,8 @@ class ImageEncodingServiceStub(object):
         """
         self.encode_image = channel.unary_unary(
                 '/ImageEncodingService/encode_image',
-                request_serializer=proto_dot_image__encoding__service__pb2.ImageRequest.SerializeToString,
-                response_deserializer=proto_dot_image__encoding__service__pb2.ImageResponse.FromString,
+                request_serializer=shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageRequest.SerializeToString,
+                response_deserializer=shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageResponse.FromString,
                 _registered_method=True)
 
 
@@ -55,8 +55,8 @@ def add_ImageEncodingServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
             'encode_image': grpc.unary_unary_rpc_method_handler(
                     servicer.encode_image,
-                    request_deserializer=proto_dot_image__encoding__service__pb2.ImageRequest.FromString,
-                    response_serializer=proto_dot_image__encoding__service__pb2.ImageResponse.SerializeToString,
+                    request_deserializer=shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageRequest.FromString,
+                    response_serializer=shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -84,8 +84,8 @@ class ImageEncodingService(object):
             request,
             target,
             '/ImageEncodingService/encode_image',
-            proto_dot_image__encoding__service__pb2.ImageRequest.SerializeToString,
-            proto_dot_image__encoding__service__pb2.ImageResponse.FromString,
+            shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageRequest.SerializeToString,
+            shared__contracts_dot_protos_dot_image__encoding__service_dot_image__encoding__service__pb2.ImageResponse.FromString,
             options,
             channel_credentials,
             insecure,
